e256cc273495bb39bbc795d2d8ebf866b933c707
jirapongse <jirapongse@hotmail.com>
2017-08-22 13:34:17 +0700

    Move response types from common.go to response.go

--- common.go
+++ common.go
@@ -48,30 +48,7 @@ type InstrumentIdentifierList struct {
 	UseUserPreferencesForValidationOptions bool                         `json:",omitempty"`
 }
 
-//RequestTokenResponse : The HTTP response from Authentication/RequestToken request will be decoded to this type by json.Unmarshal
-type RequestTokenResponse struct {
-	//The value in @odata.content field will be decoded to Metadata field
-	Metadata string `json:"@odata.context,omitempty"`
-	Value    string
-}
 
-//IdentifierValidationError : If the indentifier in the request is invalid, the repsonse from extraction (RawExtractionResult) will contain this information.
-type IdentifierValidationError struct {
-	//The value in '@odata.content' field will be decoded to 'Metadata' field
-	Metadata   string `json:"@odata.context,omitempty"`
-	Identifier InstrumentIdentifier
-	Message    string
-}
-
-//RawExtractionResult : The HTTP response from the completed Extractions/ExtractRaw request will be decoded to this type by json.Unmarshal
-type RawExtractionResult struct {
-	//The value in '@odata.content' field will be decoded to this 'Metadata' field
-	Metadata string `json:"@odata.context,omitempty"`
-	//The value in 'JobId' field will be decoded to this 'JobID field
-	JobID                      string `json:"JobId"`
-	Notes                      []string
-	IdentifierValidationErrors []IdentifierValidationError
-}
 
 //Credential : The type is used Authentication/RequestToken request. It will be encoded to JSON by Marshaller
 type Credential struct {
